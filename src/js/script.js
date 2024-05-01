75e26e58910795d33435784d7107b29629a94811
kanan <kanansnote@gmail.com>
2024-05-01 12:19:09 +0400

    Completed the task with improving the written code.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 // p301 task
 
 // first task
+alert("Note: In Edge, refreshing page doesn't reset the whole operation, but in Firefox does.");
+
+alert('Task 1: Open the console to see the first task output.')
 console.log("JS Intro".length);
 
 // second task
@@ -10,19 +13,29 @@ const rectangleLength = 15;
 
 const rectangleArea = rectangleLength * rectangleWidth;
 
-alert(`Rectangle area is ${rectangleArea}`);
+alert(`Task 2: We set rectangle width as 10 and length as 15. Then, rectangle area is going to be ${rectangleArea}.`);
 
 // third task
 
-const x = parseFloat(prompt('Enter triangle left side value')); // triangleLeft
-const y = parseFloat(prompt('Enter triangle right side value')); // triangleRight
-const z = parseFloat(prompt('Enter triangle bottom side value')); // triangleBottom
+alert("Task 3: Let's calculate a triangle's area with Heron's formula.");
 
-if (!isNaN(x) && !isNaN(y) && !isNaN(z) && x > 0 && y > 0 && z > 0) {
-    const s = 1 / 2 * (x + y + z); // semiperimeter
-    const aRoot = s * (s - x) * (s - y) * (s - z); // area root
-    const a = Math.sqrt(aRoot).toFixed(4); // area
-    alert('Triangle area: ' + a); // result
-} else {
-    alert('Invalid input. Please enter valid positive numbers for all sides.');
+let isValid = false; // Setting valid triangle as false
+
+while (!isValid) {
+    const x = parseFloat(prompt('Enter triangle left side value')); // triangleLeft
+    const y = parseFloat(prompt('Enter triangle right side value')); // triangleRight
+    const z = parseFloat(prompt('Enter triangle bottom side value')); // triangleBottom
+
+    // Check for valid triangle
+    if ((isNaN(x) || isNaN(y) || isNaN(z))) {
+        alert("One of the sides' value is missing or the value is not a number.");
+    } else if (x + y <= z || x + z <= y || y + z <= x) {
+        alert("Invalid triangle sides.");
+    } else {
+        isValid = true; // Triangle is valid, exit the loop
+        const s = 1 / 2 * (x + y + z); // semiperimeter
+        const aRoot = s * (s - x) * (s - y) * (s - z); // area root
+        const a = Math.sqrt(aRoot).toFixed(4); // area
+        alert('Triangle area: ' + a); // result
+    }
 }
